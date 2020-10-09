c4f6cce6e3656e8dd1fd91ddf705c296ef4eb178
Nefedyev Andrey <andreynef@yandex.ru>
2020-10-09 15:50:36 +0400

    test

--- src/components/CardsList/Card/Controls/LikesCounter/LikesCounter.js
+++ src/components/CardsList/Card/Controls/LikesCounter/LikesCounter.js
@@ -3,10 +3,12 @@ import styles from './LikesCounter.module.css';
 import HeartIconUnpressed from "../../../../../assets/images/HeartIconUnpressed.svg";
 
 export function LikesCounter({likes, pressed, setLikedId, likePhoto, id}) {
+
+
   return (
     <div className={styles.likesCounter}>
       <span className={styles.likesValue}>{likes}</span>
-      <button className={styles.button} onClick={()=>{console.log('btn is pressed'); likePhoto(id)}}>
+      <button className={styles.button} onClick={likePhoto(id)}>
         <img src={HeartIconUnpressed} alt={'heart'}/>
       </button>
     </div>

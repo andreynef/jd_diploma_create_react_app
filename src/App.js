17677178986edf5301d1320163d4634f61872931
Nefedyev Andrey <andreynef@yandex.ru>
2020-10-09 16:05:28 +0400

    test

--- src/App.js
+++ src/App.js
@@ -15,6 +15,11 @@ const App = () => {
   const [open, setOpen] = useState(false);
   const [pressed, setPressed] = useState(false);
   const [code, setCode] = useState('');
+  const [unsplash, setUnsplash] = useState(new Unsplash({
+    accessKey: "sQ_OK-FHQD1dS6L4h98HkNOr-HHHKRE8KuUPVf9BXAw",// accesskey из настроек вашего приложения
+    secret: "Eu_hWiHa3mUGcHyGtq2Idfj_gGCGYq6Jp0mv1ZL_kjA",// Application Secret из настроек вашего приложения
+    callbackUrl: "https://jsdiploma.nef-an.ru/auth",// Полный адрес страницы авторизации приложения (Redirect URI). Важно: этот адрес обязательно должен быть указан в настройках приложения на сайте Unsplash API/Developers
+  }));
   const [unsplashState, setUnsplashState] = useState(new Unsplash({
     accessKey: "sQ_OK-FHQD1dS6L4h98HkNOr-HHHKRE8KuUPVf9BXAw",// accesskey из настроек вашего приложения
     secret: "Eu_hWiHa3mUGcHyGtq2Idfj_gGCGYq6Jp0mv1ZL_kjA",// Application Secret из настроек вашего приложения

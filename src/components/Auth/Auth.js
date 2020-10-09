23ee11f14efc46acca5799d85602a4ff319b6bf8
Nefedyev Andrey <andreynef@yandex.ru>
2020-10-09 11:45:39 +0400

    code ready string test

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -2,21 +2,22 @@ import React from 'react';
 import Unsplash, {toJson} from 'unsplash-js';
 import {CardList} from "../CardsList/CardList";
 
+const unsplash = new Unsplash({// Создаем экземпляр объекта для доступа к API
+  accesskey: "sQ_OK-FHQD1dS6L4h98HkNOr-HHHKRE8KuUPVf9BXAw",// accesskey из настроек вашего приложения
+  secret: "Eu_hWiHa3mUGcHyGtq2Idfj_gGCGYq6Jp0mv1ZL_kjA",// Application Secret из настроек вашего приложения
+  callbackUrl: "https://jsdiploma.nef-an.ru/auth"// Полный адрес страницы авторизации приложения (Redirect URI).
+});
+console.log(`new unsplash is done`)
+
+const code = window.location.search.split('code=')[1];// Считываем GET-параметр code из URL// www.example.com/auth?code=abcdef123456...
+console.log('got code from url:', code)
+
 export function Auth({add,images, getImageObj, pressed, setPressed, setLikedId}) {
   console.log(`Auth component is working...`);
   console.log('btw check unsplash.auth:', unsplash.auth);
-
-  const unsplash = new Unsplash({// Создаем экземпляр объекта для доступа к API
-    accesskey: "sQ_OK-FHQD1dS6L4h98HkNOr-HHHKRE8KuUPVf9BXAw",// accesskey из настроек вашего приложения
-    secret: "Eu_hWiHa3mUGcHyGtq2Idfj_gGCGYq6Jp0mv1ZL_kjA",// Application Secret из настроек вашего приложения
-    callbackUrl: "https://jsdiploma.nef-an.ru/auth"// Полный адрес страницы авторизации приложения (Redirect URI).
-  });
-  console.log(`new unsplash is done`)
-
-  const code = window.location.search.split('code=')[1];// Считываем GET-параметр code из URL// www.example.com/auth?code=abcdef123456...
-  console.log('got code from url:', code)
+  console.log('code from Auth component:', code)
   if (code) {// Если код передан,...
-    console.log(`if code is in process...`)
+    console.log(`"if code" is in process...`)
     unsplash.auth.userAuthentication(code)//отправляем запрос на получение токена
       .then(res =>{
         const response = res.json();

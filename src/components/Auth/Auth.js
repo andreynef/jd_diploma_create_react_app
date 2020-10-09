33d480757c25baa13e78fb8bcd42aa7373eed937
Nefedyev Andrey <andreynef@yandex.ru>
2020-10-09 14:29:39 +0400

    test auth copy of list

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -51,7 +51,7 @@ export function Auth({add,images, getImageObj, pressed, setPressed, setLikedId, 
       <Link to={'/'}>
         click me to go back to home page
       </Link>
-      <button type={'button'} onclose={()=>getUserProfile}>get user profile</button>
+      <button type={'button'} onClick={()=>getUserProfile}>get user profile</button>
     </>
   )
 }
